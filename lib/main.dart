57ca926351f26bdb04b83f648b42501a554701e4
asus <bunmanet08@gmail.com>
2026-08-29 11:18:58 +0700

    change everything

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:post_mobile_application/route/app_route.dart';
+import 'package:post_mobile_application/route/app_route_name.dart';
+import 'package:post_mobile_application/routes/app_route_name.dart';
+import 'package:post_mobile_application/routes/app_routes.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      initialRoute: AppRouteName.splash,
+      getPages: AppRoutes.getAllRoutes(),
+      title: 'Post Mobile Application',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan),
+      ),
+    );
+  }
+}
